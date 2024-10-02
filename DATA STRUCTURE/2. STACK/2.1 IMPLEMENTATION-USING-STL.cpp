209236b15484c13662a6adf2959d7f11c7b5cc51
Abhishree Gahlot <117459964+AbhishreeGahlot@users.noreply.github.com>
2024-10-02 17:54:24 +0530

    Update 2.1 IMPLEMENTATION-USING-STL.cpp

--- DATA STRUCTURE/2. STACK/2.1 IMPLEMENTATION-USING-STL.cpp
+++ DATA STRUCTURE/2. STACK/2.1 IMPLEMENTATION-USING-STL.cpp
@@ -1,1 +1,27 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+int main()
+{
+    stack<int> s;
+    s.push(1);
+    s.push(2);
+    s.pop();
+    cout<<s.top()<<"\n";
+    cout<<s.size()<<"\n";
+    if(s.empty())
+    {
+        cout<<"stack empty"<<"\n";
+    }
+    else
+    {
+        cout<<"empty"<<"\n";
+    }
+    return 0;
+}
 
+
+//output 
+1
+1
+empty
